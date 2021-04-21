1543daa8f30548a22e726fe5f3f4d3f58604924b
MokoMaracucho <desmoulin.federico@gmail.com>
2021-04-21 21:47:49 +0200

    Spring Initializr

---
+++ src/main/java/com/oc/bibliovilleWS/BibliovilleWsApplication.java
@@ -0,0 +1,13 @@
+package com.oc.bibliovilleWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliovilleWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliovilleWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oc/bibliovilleWS/BibliovilleWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc.bibliovilleWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliovilleWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
